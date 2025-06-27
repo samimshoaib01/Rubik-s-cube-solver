832b8a37b9685fe92f743c2f82dae4fcd84a9e3b
Shoaib samim <samimshoaib01@gmail.com>
2025-06-27 10:14:49 +0530

    Add template-based PatternDatabaseEstimate class with dummy heuristic

---
+++ Model/PatternDatabase/PatternDatabase.h
@@ -0,0 +1,19 @@
+//
+// Created by shoaib-samim on 6/27/25.
+//
+
+#ifndef PATTERNDATABASE_H
+#define PATTERNDATABASE_H
+
+template<typename T>
+class PatternDatabaseEstimate {
+private:
+
+public:
+    int getEstimate(T rubiksCube) {
+        return 0;
+    }
+
+};
+
+#endif //PATTERNDATABASE_H
